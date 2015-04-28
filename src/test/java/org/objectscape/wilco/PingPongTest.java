3dc110d4e8699ce1ed2e4da25a069cce0b7343c0
Oliver Plohmann <oliver@objectscape.org>
2015-04-28 22:10:04 +0200

    created PingPongTest.pingPongPseudoDeadlockDetection()

--- src/test/java/org/objectscape/wilco/PingPongTest.java
+++ src/test/java/org/objectscape/wilco/PingPongTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.objectscape.wilco.model.Ball;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Nutzer on 23.04.2015.
@@ -21,7 +23,7 @@ public class PingPongTest {
         player("ping", table);
         player("pong", table);
 
-        table.send(new Ball());
+        table.send(new Ball()); // game on; toss the ball
 
         Thread.sleep(2000);
 
@@ -29,6 +31,29 @@ public class PingPongTest {
             System.out.println("grab the ball and don't re-send it; game over");
             table.close(ball);
         }).get();
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void pingPongPseudoDeadlockDetection() throws InterruptedException, ExecutionException, TimeoutException {
+        // modeled after this sample in Go: http://talks.golang.org/2013/advconc.slide#7
+
+        Channel<Ball> table = wilco.createChannel(false);
+
+        player("ping", table);
+        player("pong", table);
+
+        // Line below quoted out to create "deadlock", which is not detected at compile time as in the
+        // sample in Go, but nevertheless the timeout indicates that something went wrong.
+
+        // table.send(new Ball()); // game on; toss the ball
+
+        Thread.sleep(2000);
+
+        table.onReceive(ball -> {
+            System.out.println("grab the ball and don't re-send it; game over");
+            table.close(ball);
+        }).get(1, TimeUnit.SECONDS);  // timeout after 1 second indicates that something went wrong
+
     }
 
     private void player(String name, Channel<Ball> table) {
