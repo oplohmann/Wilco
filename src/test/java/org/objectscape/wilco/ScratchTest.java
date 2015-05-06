56a6c274b52fb2fa8a2f1222130d3d335cb4b0b9
oplohmann <oliver@objectscape.org>
2015-05-06 17:18:40 +0200

    added skecthy solution to ScratchTest

--- src/test/java/org/objectscape/wilco/ScratchTest.java
+++ src/test/java/org/objectscape/wilco/ScratchTest.java
@@ -2,6 +2,8 @@ package org.objectscape.wilco;
 
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 /**
  * Created by Nutzer on 27.03.2015.
  */
@@ -10,8 +12,38 @@ public class ScratchTest {
     @Test
     public void scratch() throws InterruptedException {
 
-        Alternation alternation = Alternation.Random;
-        System.out.println(Alternation.Cyclic.equals(alternation));
+        Thread myThread = new Thread(()-> {
+            System.out.println("foo");
+        });
+        Thread thread = Thread.currentThread();
+        // System.out.println(thread.equals(thread));
+        // System.out.println(myThread.equals(thread));
+
+        AtomicMarkableReference<Thread> atomicRef = new AtomicMarkableReference(null, false);
+        boolean[] markHolder = new boolean[1];
+        System.out.println(markHolder[0]);
+
+        while(!atomicRef.isMarked()) {
+            atomicRef.attemptMark(null, true);
+        }
+
+        System.out.println(atomicRef.isMarked());
+        atomicRef.get(markHolder);
+        System.out.println(markHolder[0]);
+
+        Thread currentThread = Thread.currentThread();
+        try {
+            while(!atomicRef.compareAndSet(null, currentThread, false, false)) {
+                if(atomicRef.isMarked()) {
+                    throw new RuntimeException("queue closed");
+                } else {
+                    System.out.println("other thread won");
+                }
+            }
+            // do my stuff inside critical section
+        } finally {
+            atomicRef.set(null, false); // leave critical section
+        }
 
     }
 }
