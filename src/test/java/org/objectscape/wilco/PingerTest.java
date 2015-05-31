688244f04b5400845d46d6b8b7f72ab1e43b8789
Oliver Plohmann <oliver@objectscape.org>
2015-05-31 15:39:24 +0200

    improved test case

--- src/test/java/org/objectscape/wilco/PingerTest.java
+++ src/test/java/org/objectscape/wilco/PingerTest.java
@@ -51,28 +51,67 @@ public class PingerTest extends AbstractTest {
 
     @Test
     public void pinging() throws InterruptedException, ExecutionException {
+        List<String> output = new Vector<>();
+        Channel<String> channel = wilco.createChannel();
+        channel.onReceive(message -> output.add(message));
+
+        CountDownLatch latch = new CountDownLatch(2);
+        int iters = 15;
+
+        wilco.execute(() -> pinger(channel, iters, latch));
+        wilco.execute(() -> pinger(channel, iters, latch));
+
+        channel.waitTillClosed();
+        Assert.assertEquals(iters * 2, output.size());
+    }
+
+    @Test
+    public void pingingMultiple() throws InterruptedException, ExecutionException {
+        // some monkey testing to see whether it runs very often without race conditions
         CountDownLatch mainLatch = new CountDownLatch(1);
-        int loops = 500;
+        int loops = 1000;
         for (int i = 0; i < loops; i++) {
-            List<String> output = new Vector<>();
-            Channel<String> channel = wilco.createChannel();
-            channel.onReceive(message -> output.add(message));
-
-            CountDownLatch latch = new CountDownLatch(2);
-            int iters = 3;
-
-            wilco.execute(() -> pinger(channel, iters, latch));
-            wilco.execute(() -> pinger(channel, iters, latch));
-
-            channel.waitTillClosed();
-            Assert.assertEquals(iters * 2, output.size());
-
             System.out.println(i);
+            pinging();
             if(i + 1 == loops) {
                 mainLatch.countDown();
             }
         }
 
+        mainLatch.await();
+    }
+
+    @Test
+    public void pingingDeferred() throws InterruptedException, ExecutionException {
+        List<String> output = new Vector<>();
+        Channel<String> channel = wilco.createChannel();
+        channel.onReceive(message -> output.add(message));
+
+        CountDownLatch latch = new CountDownLatch(2);
+        int iters = 3;
+
+        wilco.execute(() -> pinger(channel, iters, latch)); // 1
+        wilco.execute(() -> pinger(channel, iters, latch)); // 2
+
+        // called after 1 & 2 have started running --> deferred
+        wilco.execute(() -> printer(channel, output));
+
+        channel.waitTillClosed();
+        Assert.assertEquals(iters * 2, output.size());
+    }
+
+    @Test
+    public void pingingDeferredMultiple() throws InterruptedException, ExecutionException {
+        // some monkey testing to see whether it runs very often without race conditions
+        CountDownLatch mainLatch = new CountDownLatch(1);
+        int loops = 1000;
+        for (int i = 0; i < loops; i++) {
+            System.out.println(i);
+            pingingDeferred();
+            if(i + 1 == loops) {
+                mainLatch.countDown();
+            }
+        }
         mainLatch.await();
     }
 
@@ -85,5 +124,9 @@ public class PingerTest extends AbstractTest {
             latch.await();
         } catch (InterruptedException e) { }
         channel.close();
+    }
+
+    private void printer(Channel<String> channel, List<String> outpout) {
+        channel.onReceive(message -> outpout.add(message));
     }
 }
